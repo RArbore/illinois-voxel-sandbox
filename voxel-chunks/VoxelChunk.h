fbe4d204c9d1888b54fd89054111a7aecf8c86b4
goldartfrog <ajwefel@gmail.com>
2023-08-27 16:21:41 -0500

    began layout for voxelchunks, added glm for vector library

---
+++ voxel-chunks/VoxelChunk.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+class VoxelChunk {
+    public:
+        VoxelChunk CreateChunk();
+        VoxelChunk CreateChunk(float x, float y, float z, int w, int d, int h);
+        VoxelChunk CreateChunk(glm::vec3 pos, glm::vec3 dim);
+        enum Type { basic = 0, Int = 1, Float = 2, Vec3 = 3 };
+    private:
+        glm::vec3 position_;
+        std::shared_ptr<void> chunk_;
+        uint16_t CalculateSize(int w, int d, int h, Type t);
+};
