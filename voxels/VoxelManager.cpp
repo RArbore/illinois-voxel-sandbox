77fe99f4dfa6dc695957e56e4fe7a13553aa5e10
Jeffrey Liu <jeffliu604@gmail.com>
2023-08-28 15:15:15 -0500

    Initial implementation to generate hollow cube

--- voxels/VoxelManager.cpp
+++ voxels/VoxelManager.cpp
@@ -1,5 +1,36 @@
 #include "VoxelManager.h"
 
-VoxelChunk& VoxelManager::generate_test_cube(const glm::vec3& position) {
-	// temporary no-op
+VoxelManager::~VoxelManager() {
+	for (VoxelChunk* chunk : chunks_) {
+		delete chunk;
+	}
+}
+
+VoxelChunk* VoxelManager::generate_test_cube(const glm::vec3& position) {
+	RawVoxelChunk* new_chunk = new RawVoxelChunk(position);
+
+	// The corners will be written to multiple times, but this shouldn't affect anything
+	for (int z = 0; z < 8; z++) {
+		new_chunk->write_voxel(0, 0, z);
+		new_chunk->write_voxel(0, 7, z);
+		new_chunk->write_voxel(7, 0, z);
+		new_chunk->write_voxel(7, 7, z);
+	}
+
+	for (int y = 0; y < 8; y++) {
+		new_chunk->write_voxel(0, y, 0);
+		new_chunk->write_voxel(0, y, 7);
+		new_chunk->write_voxel(7, y, 0);
+		new_chunk->write_voxel(7, y, 7);
+	}
+
+	for (int x = 0; x < 8; x++) {
+		new_chunk->write_voxel(x, 0, 0);
+		new_chunk->write_voxel(x, 0, 7);
+		new_chunk->write_voxel(x, 7, 0);
+		new_chunk->write_voxel(x, 7, 7);
+	}
+
+	chunks_.push_back(new_chunk);
+	return new_chunk;
 }
--- voxels/VoxelManager.h
+++ voxels/VoxelManager.h
@@ -5,13 +5,15 @@
 #include <glm/glm.hpp>
 
 #include "VoxelChunk.h"
+#include "RawVoxelChunk.h"
 
 class VoxelManager {
 public:
 	VoxelManager() = default;
+	~VoxelManager();
 
-	VoxelChunk& generate_test_cube(const glm::vec3& position);
+	VoxelChunk* generate_test_cube(const glm::vec3& position);
 
 private:
-	std::vector<VoxelChunk> chunks_;
+	std::vector<VoxelChunk*> chunks_;
 };
