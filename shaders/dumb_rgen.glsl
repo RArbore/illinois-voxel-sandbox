b5dd93662b8ad2eb4c0959b98151365ad67df4f0
Russel Arbore <russel.jma@gmail.com>
2023-09-01 15:21:41 -0500

    Maybe fix

--- shaders/dumb_rgen.glsl
+++ shaders/dumb_rgen.glsl
@@ -14,11 +14,11 @@ layout(set = 1, binding = 0) uniform accelerationStructureEXT tlas;
 layout(location = 0) rayPayloadEXT vec4 hit;
 
 void main() {
-    float t = float(frame_index) / 500.0;
+    float t = float(frame_index) / 100.0;
     
-    vec2 normalized = vec2(gl_LaunchIDEXT.xy) / vec2(gl_LaunchSizeEXT.xy);
+    vec2 normalized = vec2(gl_LaunchIDEXT.xy) / vec2(gl_LaunchSizeEXT.xy) - 0.5;
     vec3 ray_origin = 3.0 * vec3(cos(t), 0.0, sin(t));
-    vec3 ray_direction = normalize(normalize(vec3(0.5) - ray_origin) + vec3(normalized - 0.5, 0.0));
+    vec3 ray_direction = normalize(normalize(vec3(0.5) - ray_origin) + vec3(-cos(normalized.x), normalized.y, -sin(normalized.z)));
 
     traceRayEXT(tlas, gl_RayFlagsOpaqueEXT, 0xFF, 0, 0, 0, ray_origin, 0.001, ray_direction, 10000.0, 0);
 
