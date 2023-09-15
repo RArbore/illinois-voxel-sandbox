50e82de76de6361abaadd75284c35d6bde26332e
Russel Arbore <russel.jma@gmail.com>
2023-09-15 10:04:33 -0500

    Rewrote procedural chunk gen function

--- drivers/ivs.cpp
+++ drivers/ivs.cpp
@@ -6,17 +6,17 @@ int main(int argc, char *argv[]) {
     ChunkManager chunk_manager;
     auto test_sphere_data1 = generate_basic_sphere_chunk(64, 64, 64, 32);
     auto test_sphere_data2 = generate_basic_sphere_chunk(64, 64, 64, 20);
-    auto test_cube_data1 = generate_basic_filled_chunk(8, 8, 8);
+    auto test_proc_data1 = generate_basic_procedural_chunk(16, 16, 16);
 
     VoxelChunkPtr test_sphere1 = chunk_manager.add_chunk(std::move(test_sphere_data1), 64, 64, 64, VoxelChunk::Format::Raw, VoxelChunk::AttributeSet::Color);
     VoxelChunkPtr test_sphere2 = chunk_manager.add_chunk(std::move(test_sphere_data2), 64, 64, 64, VoxelChunk::Format::Raw, VoxelChunk::AttributeSet::Color);
-    VoxelChunkPtr test_cube1 = chunk_manager.add_chunk(std::move(test_cube_data1), 8, 8, 8, VoxelChunk::Format::Raw, VoxelChunk::AttributeSet::Color);
+    VoxelChunkPtr test_proc1 = chunk_manager.add_chunk(std::move(test_proc_data1), 16, 16, 16, VoxelChunk::Format::Raw, VoxelChunk::AttributeSet::Color);
 
     auto context = create_graphics_context();
 
     auto model1 = build_model(context, test_sphere1);
     auto model2 = build_model(context, test_sphere2);
-    auto model3 = build_model(context, test_cube1);
+    auto model3 = build_model(context, test_proc1);
     glm::mat3x4 transform1 = {1.0F, 0.0F, 0.0F, 0.0F,
 			      0.0F, 1.0F, 0.0F, 0.0F,
 			      0.0F, 0.0F, 1.0F, 0.0F};
@@ -26,9 +26,9 @@ int main(int argc, char *argv[]) {
     glm::mat3x4 transform3 = {cosf(4.0F), -sinf(4.0F), 0.0F, 0.0F,
 			      sinf(4.0F), cosf(4.0F), 0.0F, 1.0F,
 			      0.0F, 0.0F, 1.0F, 0.0F};
-    glm::mat3x4 transform4 = {4.0F, 0.0F, 0.0F, 10.0F,
+    glm::mat3x4 transform4 = {4.0F, 0.0F, 0.0F, -5.0F,
 			      0.0F, 4.0F, 0.0F, 0.0F,
-			      0.0F, 0.0F, 4.0F, 20.0F};
+			      0.0F, 0.0F, 4.0F, -5.0F};
     auto object1 = build_object(context, model1, transform1);
     auto object2 = build_object(context, model2, transform2);
     auto object3 = build_object(context, model2, transform3);
--- voxels/VoxelChunkGeneration.cpp
+++ voxels/VoxelChunkGeneration.cpp
@@ -53,6 +53,31 @@ std::vector<std::byte> generate_basic_filled_chunk(uint32_t width, uint32_t heig
     return data;
 }
 
+std::vector<std::byte> generate_basic_procedural_chunk(uint32_t width, uint32_t height, uint32_t depth) {
+    std::vector<std::byte> data(width * height * depth * 4, static_cast<std::byte>(0));
+
+    for (uint32_t x = 0; x < width; ++x) {
+	for (uint32_t y = 0; y < height; ++y) {
+	    for (uint32_t z = 0; z < depth; ++z) {
+		if (densityfunction_({x, y, z}) > -30.0) {
+		    std::byte red = static_cast<std::byte>(x * 15);
+		    std::byte green = static_cast<std::byte>(y * 15);
+		    std::byte blue = static_cast<std::byte>(z * 15);
+		    std::byte alpha = static_cast<std::byte>(255);
+		    
+		    size_t voxel_idx = x + y * width + z * width * height;
+		    data.at(voxel_idx * 4) = red;
+		    data.at(voxel_idx * 4 + 1) = green;
+		    data.at(voxel_idx * 4 + 2) = blue;
+		    data.at(voxel_idx * 4 + 3) = alpha;
+		}
+	    }
+	}
+    }
+
+    return data;
+}
+
 double densityfunction_(const glm::vec3 &position) {
     const siv::PerlinNoise::seed_type seed = 123456u;
     const siv::PerlinNoise perlin1{ seed };
