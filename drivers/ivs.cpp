820d36a9b2cb985fcf79ff265dc5baf4eda7b873
Russel Arbore <russel.jma@gmail.com>
2023-10-03 09:59:32 -0500

    Separate model viewer demo into executable, use faster noise library

--- drivers/ivs.cpp
+++ drivers/ivs.cpp
@@ -4,14 +4,25 @@
 #include <voxels/VoxelChunkGeneration.h>
 
 int main(int argc, char *argv[]) {
+    auto test_proc_data1 = generate_basic_procedural_chunk(128, 128, 128);
+    std::cout << "Raw size: " << test_proc_data1.size() << " bytes.\n";
+    auto test_svo_data1 = convert_raw_to_svo(std::move(test_proc_data1), 128, 128, 128, 4);
+    std::cout << "SVO size: " << test_svo_data1.size() << " bytes.\n";
+
     ChunkManager chunk_manager;
     auto context = create_graphics_context();
+    VoxelChunkPtr test_proc1 = chunk_manager.add_chunk(
+						       std::move(test_svo_data1), 128, 128, 128, VoxelChunk::Format::SVO,
+        VoxelChunk::AttributeSet::Color);
 
-    const std::string modelsDirectory = MODELS_DIRECTORY;
-    const std::string filePath = modelsDirectory + "/monu16.vox";
-    auto test_scene = test_loader(filePath, chunk_manager, context);
+    auto model1 = build_model(context, test_proc1);
+    glm::mat3x4 transform1 = {1.0F, 0.0F, 0.0F, -64.0F,
+                              0.0F, 1.0F, 0.0F, -64.0F, 
+                              0.0F, 0.0F, 1.0F, -64.0F};
+    auto object1 = build_object(context, model1, transform1);
+    auto scene = build_scene(context, {object1});
 
     while (!should_exit(context)) {
-        render_frame(context, test_scene);
+        render_frame(context, scene);
     }
 }
--- voxels/VoxelChunkGeneration.cpp
+++ voxels/VoxelChunkGeneration.cpp
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <string>
 
-#include <external/PerlinNoise.hpp>
+#include <external/FastNoiseLite.h>
 #include <external/glm/glm/glm.hpp>
 #include <external/ogt_vox.h>
 
@@ -76,16 +76,18 @@ generate_basic_filled_chunk(uint32_t width, uint32_t height, uint32_t depth) {
 std::vector<std::byte> generate_basic_procedural_chunk(uint32_t width,
                                                        uint32_t height,
                                                        uint32_t depth) {
-    std::vector<std::byte> data(width * height * depth * 4,
+    std::vector<std::byte> data(static_cast<size_t>(width) * static_cast<size_t>(height) * static_cast<size_t>(depth) * 4,
                                 static_cast<std::byte>(0));
+    FastNoiseLite noise;
+    noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
 
     for (uint32_t x = 0; x < width; ++x) {
         for (uint32_t y = 0; y < height; ++y) {
             for (uint32_t z = 0; z < depth; ++z) {
-                if (densityfunction_({x, y, z}) > -30.0) {
-                    std::byte red = static_cast<std::byte>(x * 15);
-                    std::byte green = static_cast<std::byte>(y * 15);
-                    std::byte blue = static_cast<std::byte>(z * 15);
+                if (noise.GetNoise(static_cast<float>(x), static_cast<float>(y), static_cast<float>(z)) > 0.0F) {
+                    std::byte red = static_cast<std::byte>(255);
+                    std::byte green = static_cast<std::byte>(255);
+                    std::byte blue = static_cast<std::byte>(255);
                     std::byte alpha = static_cast<std::byte>(255);
 
                     size_t voxel_idx = x + y * width + z * width * height;
@@ -99,25 +101,6 @@ std::vector<std::byte> generate_basic_procedural_chunk(uint32_t width,
     }
 
     return data;
-}
-
-double densityfunction_(const glm::vec3 &position) {
-    const siv::PerlinNoise::seed_type seed = 123456u;
-    const siv::PerlinNoise perlin1{seed};
-    const siv::PerlinNoise perlin2{seed + 1};
-    // const siv::PerlinNoise perlin3{ seed + 2 };
-    const double noise1 =
-        perlin1.noise3D(position.x * 0.01, position.y * 0.01,
-                        position.z * 0.01); // scale them down for the noise
-    const double noise2 =
-        perlin2.noise3D(position.x * 0.1, position.y * 0.1, position.z * 0.1);
-    double density =
-        position.y - 40; // origin seems to be at top corner of chunk
-    // density += std::sin(position.x/ 100) * 10;
-    // density += std::cos(position.y / 100) * 15;
-    density += noise1 * 5;
-    density += noise2 * 10;
-    return density;
 }
 
 std::vector<std::byte> generate_terrain(uint32_t width, uint32_t height,
