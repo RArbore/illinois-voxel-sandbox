75713c66b049db77dbe4ba5034ff0b29467720e1
Russel Arbore <russel.jma@gmail.com>
2023-10-14 12:05:55 -0500

    Add object with different model

--- drivers/ivs.cpp
+++ drivers/ivs.cpp
@@ -11,6 +11,8 @@ int main(int argc, char *argv[]) {
         convert_raw_to_svo(std::move(test_proc_data1), 128, 128, 128, 4);
     std::cout << "SVO size: " << test_svo_data1.size() << " bytes.\n";
 
+    auto test_sphere_data2 = generate_basic_sphere_chunk(64, 64, 64, 32);
+
     ChunkManager chunk_manager;
     auto window = create_window();
     auto context = create_graphics_context(window);
@@ -20,14 +22,18 @@ int main(int argc, char *argv[]) {
     VoxelChunkPtr test_proc1 = chunk_manager.add_chunk(
         std::move(test_svo_data1), 128, 128, 128, VoxelChunk::Format::SVO,
         VoxelChunk::AttributeSet::Color);
+    VoxelChunkPtr test_sphere2 = chunk_manager.add_chunk(
+        std::move(test_sphere_data2), 64, 64, 64, VoxelChunk::Format::Raw,
+        VoxelChunk::AttributeSet::Color);
 
     auto model1 = build_model(context, test_proc1);
+    auto model2 = build_model(context, test_sphere2);
     glm::mat3x4 transform1 = {1.0F, 0.0F,   0.0F, -64.0F, 0.0F, 1.0F,
                               0.0F, -64.0F, 0.0F, 0.0F,   1.0F, -64.0F};
     auto object1 = build_object(context, model1, transform1);
     glm::mat3x4 transform2 = {1.0F, 0.0F,  0.0F, 64.0F, 0.0F, 1.0F,
                               0.0F, 64.0F, 0.0F, 0.0F,  1.0F, 64.0F};
-    auto object2 = build_object(context, model1, transform2);
+    auto object2 = build_object(context, model2, transform2);
 
     auto scene = build_scene(context, {object1});
     add_object(context, scene, object2);
