814d439dee29075220291555a333d8ab95cc248e
Russel Arbore <russel.jma@gmail.com>
2023-11-12 15:44:32 -0600

    More precise chunk requests

--- shaders/Raw_Color_rint.glsl
+++ shaders/Raw_Color_rint.glsl
@@ -5,33 +5,6 @@
 
 #include "common.glsl"
 
-struct aabb_intersect_result {
-    float front_t;
-    float back_t;
-    uint k;
-};
-
-aabb_intersect_result hit_aabb(const vec3 minimum, const vec3 maximum, const vec3 origin, const vec3 direction) {
-    aabb_intersect_result r;
-    vec3 invDir = 1.0 / direction;
-    vec3 tbot = invDir * (minimum - origin);
-    vec3 ttop = invDir * (maximum - origin);
-    vec3 tmin = min(ttop, tbot);
-    vec3 tmax = max(ttop, tbot);
-    float t0 = max(tmin.x, max(tmin.y, tmin.z));
-    float t1 = min(tmax.x, min(tmax.y, tmax.z));
-    r.front_t = t1 > max(t0, 0.0) ? t0 : -FAR_AWAY;
-    r.back_t = t1 > max(t0, 0.0) ? t1 : -FAR_AWAY;
-    if (t0 == tmin.x) {
-	r.k = tbot.x > ttop.x ? 1 : 0;
-    } else if (t0 == tmin.y) {
-	r.k = tbot.y > ttop.y ? 3 : 2;
-    } else if (t0 == tmin.z) {
-	r.k = tbot.z > ttop.z ? 5 : 4;
-    }
-    return r;
-}
-
 void main() {
     uint volume_id = gl_InstanceCustomIndexEXT;
 
