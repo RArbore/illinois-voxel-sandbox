2a18ec53e1eb583dfd27305e4948561426de3b54
Jeffrey Liu <jeffliu604@gmail.com>
2023-11-24 14:17:04 -0500

    Swap to luminance extended Reinhard

--- shaders/tonemap_comp.glsl
+++ shaders/tonemap_comp.glsl
@@ -8,14 +8,24 @@ layout (binding = 3, rgba8) uniform readonly image2D image_positions;
 
 layout (local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
-// Referenced from https://github.com/NVIDIAGameWorks/Falcor/blob/master/Source/RenderPasses/ToneMapper/ToneMapping.ps.slang
-float color_luminance(vec3 color) {
-    return dot(color, vec3(0.299, 0.587, 0.114));
+// Referenced from https://64.github.io/tonemapping/#extended-reinhard-luminance-tone-map
+float luminance(vec3 color) {
+    return dot(color, vec3(0.2126f, 0.7152f, 0.0722f));
 }
 
+vec3 change_luminance(vec3 initial_color, float final_luminance) {
+    float initial_luminance = luminance(initial_color);
+    return initial_color * (final_luminance / initial_luminance);
+}
+
+// Reinhard extended luminance
 vec3 reinhard(vec3 color) {
-    float luminance = color_luminance(color);
-    return color / (luminance + 1);
+    const float white_point = 1.0f;
+
+    float initial_luminance = luminance(color);
+    float factor = initial_luminance * (1.0f + (initial_luminance / (white_point * white_point)));
+    float final_luminance = factor / (1.0f + initial_luminance);
+    return change_luminance(color, final_luminance);
 }
 
 void main()
