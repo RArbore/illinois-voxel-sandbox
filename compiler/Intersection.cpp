116e2172f50600ad49f41ef90a4751327a3c6b49
Russel Arbore <russel.jma@gmail.com>
2024-02-12 14:27:58 -0600

    Generate some DDA initialization code

--- compiler/Intersection.cpp
+++ compiler/Intersection.cpp
@@ -34,8 +34,28 @@ bool intersect_format_)" << i << R"((uint volume_id, uint node_id, vec3 obj_ray_
 	auto [inc_w, inc_h, inc_d] = calculate_bounds(format, i);
 	auto this_w = inc_w / sub_w, this_h = inc_h / sub_h, this_d = inc_d / sub_d;
 
+	ss << R"(    float sub_w = )" << sub_w << R"(.0;
+    float sub_h = )" << sub_h << R"(.0;
+    float sub_d = )" << sub_d << R"(.0;
+    float inc_w = )" << inc_w << R"(.0;
+    float inc_h = )" << inc_h << R"(.0;
+    float inc_d = )" << inc_d << R"(.0;
+    float this_w = )" << this_w << R"(.0;
+    float this_h = )" << this_h << R"(.0;
+    float this_d = )" << this_d << R"(.0;
+
+)";
+
 	switch (format[i].format_) {
 	case Format::Raw:
+	    ss << R"(    vec3 chunk_ray_pos = obj_ray_pos / vec3(sub_w, sub_h, sub_d);
+    vec3 chunk_ray_dir = obj_ray_dir;
+    vec3 chunk_ray_intersect_point = chunk_ray_pos + chunk_ray_dir * max(t, 0.0) / vec3(sub_w, sub_h, sub_d);
+    ivec3 chunk_ray_voxel = ivec3(min(chunk_ray_intersect_point, ivec3()" << (this_w - 1) << R"(, )" <<  (this_h - 1) << R"(, )" << (this_d - 1) << R"()));
+    ivec3 chunk_ray_step = ivec3(sign(chunk_ray_dir));
+    vec3 chunk_ray_delta = abs(vec3(length(chunk_ray_dir)) / chunk_ray_dir);
+    vec3 chunk_side_dist = (sign(chunk_ray_dir) * (vec3(chunk_ray_voxel) - chunk_ray_intersect_point) + (sign(chunk_ray_dir) * 0.5) + 0.5) * chunk_ray_delta;
+)";
 	    break;
 	default:
 	    ASSERT(false, "Unhandled format.");
