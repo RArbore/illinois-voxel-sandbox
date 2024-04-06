36d52bf1ca6a1b055ab066b796856dcbff06e3b9
Russel Arbore <russel.jma@gmail.com>
2024-04-06 17:45:33 -0500

    emit separate graphs

--- experiments/assemble_figures.py
+++ experiments/assemble_figures.py
@@ -81,75 +81,44 @@ for datapoint in datapoints:
 colors = ["#FF0000", "#00FF00", "#0000FF", "#FF00FF"]
 lightcolors = ["#FF7F7F", "#7FFF7F", "#7F7FFF", "#FF7FFF"]
 
-plt.figure(figsize=(6, 5))
-for model_idx in range(0, num_models):
-    x, y = data[:NUM_4K_FORMATS, model_idx, 0], data[:NUM_4K_FORMATS, model_idx, 1]
+def plot_model_at_resolution(data, model_idx, resolution, s):
+    plt.figure(figsize=(6, 5))
+    x, y = data[:, 0], data[:, 1]
     pareto = [all([(tx <= ox or ty >= oy) for ox, oy in zip(x, y)]) for tx, ty in zip(x, y)]
-    points = list(zip(data[:NUM_4K_FORMATS, model_idx, 0], data[:NUM_4K_FORMATS, model_idx, 1]))
+    points = list(zip(data[:, 0], data[:, 1]))
     indices = list(filter(lambda idx: pareto[idx], argsort(points)))
     p1 = [points[idx][0] for idx in indices]
     p2 = [points[idx][1] for idx in indices]
     plt.plot(p1, p2, c="gray", linestyle='dashed')
+    plt.scatter(x, y, c=[colors[model_idx] if p else lightcolors[model_idx] for p in pareto], s=[float(p) * 25.0 + 15.0 for p in pareto])
 
-for model_idx in range(0, num_models):
-    x, y = data[:NUM_4K_FORMATS, model_idx, 0], data[:NUM_4K_FORMATS, model_idx, 1]
-    pareto = [all([(tx <= ox or ty >= oy) for ox, oy in zip(x, y)]) for tx, ty in zip(x, y)]
-    plt.scatter(x, y, c=[colors[model_idx] if p else lightcolors[model_idx] for p in pareto], label=models[model_idx], s=[float(p) * 25.0 + 15.0 for p in pareto])
-
-texts = []
-for model_idx in range(0, num_models):
-    for format_idx in range(0, NUM_4K_FORMATS):
-        x, y = data[format_idx, model_idx, 0], data[format_idx, model_idx, 1]
+    texts = []
+    for format_idx in range(s[0], s[1]):
+        x, y = data[format_idx - s[0], 0], data[format_idx - s[0], 1]
         if y == 0.0 and formats[format_idx].startswith("SV") and models[model_idx] == "sponza":
             y = format_idx / 1000.0
-        texts.append(plt.text(x, y, write_roman(format_idx + 1)))
+        texts.append(plt.text(x, y, write_roman(format_idx + 1 - s[0])))
 
-for format_idx in range(0, NUM_4K_FORMATS):
-    print(formats[format_idx], write_roman(format_idx + 1))
+    plot_cutoff = 0.0 in data[:, 1]
+    if plot_cutoff:
+        plt.axvline(2 ** 32)
+        plt.text((2 ** 32) * 1.01, 2, 'GPU Size Cutoff (4 GiB)', rotation=270)
+    
+    plt.xscale('log')
+    plt.xlabel("Model Size (bytes)")
+    plt.ylabel("Ray Intersection Performance (FPS)")
+    plt.title("Rendering Performance vs. Compression for " + str(models[model_idx]) + " at " + resolution + " resolution")
+    adjust_text(texts)
+    plt.show()
 
-plot_cutoff = 0.0 in data[:NUM_4K_FORMATS, :, 1]
-if plot_cutoff:
-    plt.axvline(2 ** 32)
-    plt.text((2 ** 32) * 1.01, 2, 'GPU Size Cutoff (4 GiB)', rotation=270)
+slices = {"4096^3" : [0, NUM_4K_FORMATS], "512^3" : [NUM_4K_FORMATS, num_formats]}
+for resolution in ["4096^3", "512^3"]:
+    s = slices[resolution]
+    for model_idx in range(0, num_models):
+        d = data[s[0]:s[1], model_idx]
+        plot_model_at_resolution(d, model_idx, resolution, s)
+    
+    for format_idx in range(s[0], s[1]):
+        print(formats[format_idx], write_roman(format_idx + 1 - s[0]))
 
-plt.xscale('log')
-plt.xlabel("Model Size (bytes)")
-plt.ylabel("Ray Intersection Performance (FPS)")
-plt.title("Rendering Performance vs. Compression for 4096^3 formats")
-adjust_text(texts)
-plt.legend()
-plt.show()
-
-print("")
-
-plt.figure(figsize=(6, 5))
-for model_idx in range(0, num_models):
-    x, y = data[NUM_4K_FORMATS:, model_idx, 0], data[NUM_4K_FORMATS:, model_idx, 1]
-    pareto = [all([(tx <= ox or ty >= oy) for ox, oy in zip(x, y)]) for tx, ty in zip(x, y)]
-    points = list(zip(data[NUM_4K_FORMATS:, model_idx, 0], data[NUM_4K_FORMATS:, model_idx, 1]))
-    indices = list(filter(lambda idx: pareto[idx], argsort(points)))
-    p1 = [points[idx][0] for idx in indices]
-    p2 = [points[idx][1] for idx in indices]
-    plt.plot(p1, p2, c="gray", linestyle='dashed')
-
-for model_idx in range(0, num_models):
-    x, y = data[NUM_4K_FORMATS:, model_idx, 0], data[NUM_4K_FORMATS:, model_idx, 1]
-    pareto = [all([(tx <= ox or ty >= oy) for ox, oy in zip(x, y)]) for tx, ty in zip(x, y)]
-    plt.scatter(x, y, c=[colors[model_idx] if p else lightcolors[model_idx] for p in pareto], label=models[model_idx], s=[float(p) * 25.0 + 15.0 for p in pareto])
-
-texts = []
-for model_idx in range(0, num_models):
-    for format_idx in range(NUM_4K_FORMATS, num_formats):
-        x, y = data[format_idx, model_idx, 0], data[format_idx, model_idx, 1]
-        texts.append(plt.text(x, y, write_roman(format_idx + 1 - NUM_4K_FORMATS)))
-
-for format_idx in range(NUM_4K_FORMATS, num_formats):
-    print(formats[format_idx], write_roman(format_idx + 1 - NUM_4K_FORMATS))
-
-plt.xscale('log')
-plt.xlabel("Model Size (bytes)")
-plt.ylabel("Ray Intersection Performance (FPS)")
-plt.title("Rendering Performance vs. Compression for 512^3 formats")
-adjust_text(texts)
-plt.legend()
-plt.show()
+    print("")
